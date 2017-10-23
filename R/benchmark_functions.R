581e1eb7408444916d9253ebb777edb64ef93e5c
xiuweiz <zhangxiuwei03@gmail.com>
2017-10-23 16:13:43 -0700

    updated benchmark_functions.R
    changed coloring scheme for plotting the number of inflection points.

--- R/benchmark_functions.R
+++ R/benchmark_functions.R
@@ -13,13 +13,23 @@ PlotTsne <- function(meta,data,plotname,label,discrete=T,saving=F){
     uniqrows<-c(1:length(data[,1]))[!duplicated(data)]
     data <- data[uniqrows,]
 	data_tsne=Rtsne(t(data))
+	
+	# if(discrete==T){
+	# 	plot_tsne=data.frame(alpha=meta[,'alpha'],beta=meta[,'beta'],sigma=meta[,'sigma'],
+	# 	pop=meta[,'pop'],label=factor(meta[,label]),x=data_tsne$Y[,1],y=data_tsne$Y[,2])		
+	# }else{
+	# 	plot_tsne=data.frame(alpha=meta[,'alpha'],beta=meta[,'beta'],sigma=meta[,'sigma'],
+	# 	pop=meta[,'pop'],label=meta[,label],x=data_tsne$Y[,1],y=data_tsne$Y[,2])
+	# }
+	
 	if(discrete==T){
-		plot_tsne=data.frame(alpha=meta[,'alpha'],beta=meta[,'beta'],sigma=meta[,'sigma'],
-		pop=meta[,'pop'],label=factor(meta[,label]),x=data_tsne$Y[,1],y=data_tsne$Y[,2])		
+	  plot_tsne=data.frame(alpha=meta[,'alpha'],
+	                       pop=meta[,'pop'],label=factor(meta[,label]),x=data_tsne$Y[,1],y=data_tsne$Y[,2])		
 	}else{
-		plot_tsne=data.frame(alpha=meta[,'alpha'],beta=meta[,'beta'],sigma=meta[,'sigma'],
-		pop=meta[,'pop'],label=meta[,label],x=data_tsne$Y[,1],y=data_tsne$Y[,2])
+	  plot_tsne=data.frame(alpha=meta[,'alpha'],
+	                       pop=meta[,'pop'],label=meta[,label],x=data_tsne$Y[,1],y=data_tsne$Y[,2])
 	}
+	
 	p <- ggplot(plot_tsne, aes(x, y))
 	p <- p + geom_point()
 	p <- p + geom_point(aes(colour = plot_tsne[['label']]))+labs(color=label)
