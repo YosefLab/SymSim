df31f258de1fb5d8f4bd198b18b5d4f60cbad70b
chenling antelope <chenlingantelope@gmail.com>
2018-01-10 12:48:53 -0800

    added more summary functions

--- R/summary_functions.R
+++ R/summary_functions.R
@@ -69,7 +69,7 @@ PlotCountHeatmap <- function(log_real, mean_counts,given_ord=NA,zeropropthres=0.
     ggtitle(sprintf('distribution of mRNA counts of %s', data_name)) +
     labs(colour = 'Percentage of Cells',x='log10(Count) bins',y='Genes') +
     scale_y_discrete(breaks=NULL) +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1)) ##
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
     if(saving==T){ggsave(filename,dev='jpeg',width = 8, height = 8)}else{return(list(ord,p))}
 }
 #' Plotting the histograms of kon,koff,s values
@@ -193,7 +193,84 @@ PlotGCLENbias <- function(filename,outputname){
     ggsave(outputname, arrangeGrob(grobs = plots))
     return(plots)
 }
+#' Calculating the 
+#'
+#' @param filename the name of the simualted robj data file
 
 cv <- function(x) {return(sd(x)/mean(x))}
 
 fano <- function(x) {return((sd(x))^2/mean(x))}
+
+percent_nonzero <- function(x) {return(sum(x>0)/length(x))}
+
+
+#' Plotting simulated FNR 
+#'
+#' @param current_counts expression matrix
+#' @param true_counts true expression matrix
+#' @param titlestr title string
+
+plotFNRsim <- function(current_counts, true_counts, x_use="true", titlestr){
+  
+  #cellsize <- colSums(current_counts)
+  zeros <- colSums(current_counts==0 & true_counts!=0)
+  #summary(zeros)
+  
+  ref.glms <- lapply(c(1:length(true_counts[1,])),function(i){
+    real <- true_counts[,i]
+    bias <- current_counts[,i]/sum(current_counts[,i])*10^6
+    fn <- (bias==0 & real!=0)
+    if (x_use == "observed")
+    {data <- data.frame(exprs=log(bias+1,10), fn=fn)} else
+      if (x_use == "true")
+      {data <- data.frame(exprs=log(real+1,10), fn=fn)}
+    data <- data[real!=0,]
+    model <- glm(fn~exprs,family=binomial(link='logit'),data=data)
+    return(model$coefficients)
+  })
+  
+  ncols <- 2000
+  colfunc <- colorRampPalette(c("blue", "green", "red", "purple"))
+  allcol <- colfunc(ncols)
+  
+  col_vec <- allcol[round(rescale2range(colSums(current_counts), 2000))]
+  
+  plot(NULL, main = sprintf("FNR Curves %s", titlestr), ylim = c(0,1),xlim = c(0,6), 
+       ylab = "Failure Probability", xlab = "Mean log10 Expression")
+  x = (0:60)/10
+  for(si in 1:length(ref.glms)){
+    y = 1/(exp(-ref.glms[[si]][1] - ref.glms[[si]][2] * x) + 1)
+    lines(x, y, type = 'l', lwd = 2, col=col_vec[si])
+  }
+}
+
+#' Plotting real data FNR 
+#'
+#' @param expr_matrix expression matrix
+#' @param hk house keeping genes
+#' @param data_name title string
+#' @param col_vec color vector
+
+plotFNRreal <- function(expr_matrix, hk, data_name, col_vec){
+  # Mean log10(x+1) expression
+  mu_obs = rowMeans(log10(expr_matrix[hk,]+1))
+  drop_outs = expr_matrix[hk,] == 0
+  
+  # Logistic Regression Model of Failure
+  ref.glms = list()
+  for (si in 1:dim(drop_outs)[2]){
+    #fit = glm(cbind(drop_outs[,si], 1 - drop_outs[,si]) ~ mu_obs,family=binomial(logit))
+    fit = glm(drop_outs[,si] ~ mu_obs,family=binomial(logit))
+    ref.glms[[si]] = fit$coefficients
+  }
+  
+  #The list ref.glm contains the intercept and slope of each fit. 
+  # We can now visualize the fit curves and the corresponding Area Under the Curves (AUCs):
+  plot(NULL, main = sprintf("FNR Curves %s", data_name), ylim = c(0,1),xlim = c(0,6), 
+       ylab = "Failure Probability", xlab = "Mean log10 Expression")
+  x = (0:60)/10
+  for(si in 1:ncol(expr_matrix)){
+    y = 1/(exp(-ref.glms[[si]][1] - ref.glms[[si]][2] * x) + 1)
+    lines(x, 1/(exp(-ref.glms[[si]][1] - ref.glms[[si]][2] * x) + 1), type = 'l', lwd = 2, col=col_vec[si])
+  }
+}
