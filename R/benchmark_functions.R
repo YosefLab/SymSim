d2a2f4455b6f40e63f30bd8448cb7030a309af76
Xiuwei Zhang <xiuwei.zhang@s128.Millennium.Berkeley.EDU>
2018-03-22 17:06:32 -0700

    updated PlotTsne and ClusterQuality functions

--- R/benchmark_functions.R
+++ R/benchmark_functions.R
@@ -7,7 +7,7 @@
 #' @param perplexity: perplexity parameter for tsne
 #' @param dims: number of dimensions kept after tsne
 
-ClusterQuality <- function(data,meta,n_pc,perplexity,dims){
+ClusterQuality <- function(data,meta,n_pc,perplexity,dims,npop=5){
   uniqcols<-c(1:length(data[1,]))[!duplicated(t(data))]
   data <- data[,uniqcols];meta <- meta[uniqcols,,drop=FALSE] 
   uniqrows<-c(1:length(data[,1]))[!duplicated(data)]
@@ -16,7 +16,7 @@ ClusterQuality <- function(data,meta,n_pc,perplexity,dims){
   data_pc <- data_pc$x[,c(1:n_pc)]
   data_tsne=Rtsne(dims = dims ,data_pc,perplexity=perplexity)
   lowdim_data <- data_tsne$Y
-  assignment <- kmeans(lowdim_data,5,nstart=20)
+  assignment <- kmeans(lowdim_data,npop,nstart=20)
   ri <- rand.index(assignment[[1]],meta$pop)
   lowdim_dist <- as.matrix(dist(lowdim_data, method = "euclidean"))
   sw <- silhouette(x=meta$pop, dist=dist(lowdim_data, method = "euclidean"))
@@ -28,10 +28,12 @@ ClusterQuality <- function(data,meta,n_pc,perplexity,dims){
   # for all clusters, which one has the highest prop of pop2 cells
   # for the most common pop2 cluster, what is the proportion of pop2 cells? 
   min_in_clst <- sapply(split(x=meta$pop,f=assignment[[1]]),function(X){
-  	sum(X%in%c(1:5)[minpop])})
+  	sum(X%in%c(1:npop)[minpop])})
   clst_w_min <- sapply(split(x=meta$pop,f=assignment[[1]]),function(X){
-  	sum(X%in%c(1:5)[minpop])/length(X)})
-  return(c(ri,sw_mean,sw_minpop,min_in_clst,clst_w_min))
+  	sum(X%in%c(1:npop)[minpop])/length(X)})
+  res <- c(ri,sw_mean,sw_minpop,min_in_clst,clst_w_min)
+  names(res) <- c("RI", "SW_mean", "SW_minpop", paste0("min_in_clst",1:npop), paste0("clst_w_min", 1:npop))
+  return(res)
 }
 #' rand index
 #'
@@ -56,22 +58,24 @@ rand.index<-function (group1, group2)
 #' @param plotname: the name of the jpeg file
 #' @param label: the column name of the meta data that the points needs to be colored by
 
-PlotTsne <- function(meta, data, evf_type, n_pc, perplexity=30, label, saving=F, plotname){
+PlotTsne <- function(meta, data, evf_type, pca = T, n_pc, perplexity=30, label, saving=F, plotname){
   library('Rtsne')
   uniqcols<-c(1:length(data[1,]))[!duplicated(t(data))]
   data <- data[,uniqcols];meta <- meta[uniqcols,,drop=FALSE] 
   uniqrows<-c(1:length(data[,1]))[!duplicated(data)]
   data <- data[uniqrows,]
-  data_pc <- prcomp(t(data))
-  data_pc <- data_pc$x[,c(1:n_pc)]
-  data_tsne=Rtsne(data_pc,perplexity=perplexity)
+  if (pca){
+    data_pc <- prcomp(t(data))
+    data <- t(data_pc$x[,c(1:n_pc)])
+  }
+  data_tsne=Rtsne(t(data),perplexity=perplexity)
   if(evf_type=="discrete" | evf_type=="one.population"){
     plot_tsne <- cbind(meta, data.frame(label=factor(meta[,label]),x=data_tsne$Y[,1],y=data_tsne$Y[,2]))
   }else{
     plot_tsne <- cbind(meta, data.frame(label=meta[,label],x=data_tsne$Y[,1],y=data_tsne$Y[,2]))
   }
   p <- ggplot(plot_tsne, aes(x, y))
-  p <- p + geom_point()
+  p <- p + geom_point(shape=20)
   p <- p + geom_point(aes(colour = plot_tsne[['label']]))+labs(color=label)
   if(saving==T){ggsave(p,filename=plotname,device='pdf',width=5,height=4)}
   if(saving==F){p <- p + ggtitle(plotname)}
